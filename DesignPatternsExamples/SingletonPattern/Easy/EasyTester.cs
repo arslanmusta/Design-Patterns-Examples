ca6e24aadcc9f979f2edc0a53e5e3c4411ce75b4
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2021-06-09 13:00:12 +0300

    add singleton pattern

---
+++ DesignPatternsExamples/SingletonPattern/Easy/EasyTester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern.Easy
+{
+    public static class EasyTester
+    {
+        public static void Test()
+        {
+            var object1 = God.Instance;
+            var object2 = God.Instance;
+
+            object1.Value = 666;
+
+            Console.WriteLine($"Object1 Value: {object1.Value}, Object2 Value: {object2.Value}");
+        }
+    }
+}
---
+++ DesignPatternsExamples/SingletonPattern/Easy/God.cs
@@ -0,0 +1,16 @@
+﻿namespace SingletonPattern.Easy
+{
+    public class God
+    {
+        private static volatile God _instance = new ();
+
+        public static God Instance => _instance;
+
+        public int Value { get; set; }
+        
+        private God()
+        {
+            // init
+        }
+    }
+}
